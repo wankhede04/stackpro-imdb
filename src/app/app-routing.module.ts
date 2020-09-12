25751a0efffc6b8a287bd2edd9a06a5ce7cdc83e
wankhede04 <wankhedevijay04@gmail.com>
2020-09-12 14:54:46 +0530

    implemented routing for all screen.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
+import { MovieDetailComponent } from './movie-detail/movie-detail.component';
+import { MovieDetailsComponent } from './movie-details/movie-details.component';
+import { RegisterComponent } from './register/register.component';
+import { UpcomingComponent } from './upcoming/upcoming.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomePageComponent },
+  { path: 'detail', component: MovieDetailComponent },
+  { path: 'details', component: MovieDetailsComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'upcoming', component: UpcomingComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
