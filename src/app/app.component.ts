7f4f024efae8fab47eb3a6e5da3c1d26ee0eda62
wankhede04 <wankhedevijay04@gmail.com>
2020-09-12 14:43:57 +0530

    Added side panel.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,16 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {}
+export class AppComponent {
+
+  constructor(private route: Router) {}
+
+  public navigateTo(route: string) {
+    this.route.navigate([route]);
+  }
+}
